ae33bf741446de72965e5d8dc02357e696d42ce5
Keegan George <kgeorge13@gmail.com>
2019-10-01 21:40:58 -0700

    [Keegan] Completed Registration Functionality
    Created the ability to register using a form which upon submit leads to a registration success page, and also stores registered data (email and password) in a text-file.

--- index.php
+++ index.php
@@ -1,25 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="utf-8"/>
-	<link rel="apple-touch-icon" sizes="76x76" href="./assets/img/favicon.png">
-	<link rel="icon" type="image/png" href="./assets/img/favicon.png">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
-	<title>F1 | Home</title>
-	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport'/>
-	<!-- Adobe Fonts -->
-	<link rel="stylesheet" href="https://use.typekit.net/qah3bmy.css">
-	<!-- Font Awesome Icons -->
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-	<!-- Main CSS -->
-	<link href="./assets/css/main.css" rel="stylesheet"/>
-	<!-- Animation CSS -->
-	<link href="./assets/css/vendor/aos.css" rel="stylesheet"/>
-</head>
+	<head>
+		<meta charset="utf-8"/>
+		<link rel="apple-touch-icon" sizes="76x76" href="./assets/img/favicon.png">
+		<link rel="icon" type="image/png" href="./assets/img/favicon.png">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+		<title>F1 | Home</title>
+		<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport'/>
+		<!-- Adobe Fonts -->
+		<link rel="stylesheet" href="https://use.typekit.net/qah3bmy.css">
+		<!-- Font Awesome Icons -->
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+		<!-- Main CSS -->
+		<link href="./assets/css/main.css" rel="stylesheet"/>
+		<!-- Animation CSS -->
+		<link href="./assets/css/vendor/aos.css" rel="stylesheet"/>
+	</head>
 
 <body>
-
-
 <!--------------------------------------
 NAVBAR
 --------------------------------------->
@@ -32,10 +30,10 @@ NAVBAR
 	<div class="navbar-collapse collapse" id="navbarColor02">
 		<ul class="navbar-nav mr-auto d-flex align-items-center">
 			<li>
-			<a class="nav-link" href="./about.php">About</a>
+				<a class="nav-link" href="./about.php">About</a>
 			</li>
 			<li class="nav-item">
-			<a class="nav-link" href="./docs.html">Races</a>
+				<a class="nav-link" href="./docs.html">Races</a>
 			</li>
 		</ul>
 		<ul class="navbar-nav ml-auto d-flex align-items-center">
@@ -337,14 +335,11 @@ CTA
 				<button type="submit" class="btn btn-primary btn-round btn-block">Sign in</button>
 				<small class="d-block mt-4 text-center"><a class="text-gray" href="#">Forgot your password?</a></small>
 			</form>
-		</div>
-	</div>
 			</div>
-
-				</form>
 			</div>
 		</div>
 	</div>
+</div>
 </div>
 
 <!--------------------------------------
