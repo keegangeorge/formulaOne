9691878090ec966d4f6993c3cac0af4360e83c9e
Keegan George <kgeorge13@gmail.com>
2019-10-29 13:50:22 -0700

    [Keegan] Added file housing basic functions
    Functions relating to short-hands, page errors, page redirects, and other basic php commands.

---
+++ private/general_functions.php
@@ -0,0 +1,73 @@
+<?php 
+    function url_for($script_path) {
+        // add the leading '/' if it isn't present
+        if ($script_path[0] != '/') {
+            $script_path = "/" . $script_path;
+        }
+        return WWW_ROOT . $script_path;
+    }
+
+    /**
+     * Shorthand function for urlencode($string);
+     */
+    function u($string="") {
+        return urlencode($string);
+    }
+
+    /**
+     * Shorthand function for rawurlencode($string);
+     */
+    function raw_u($string="") {
+        return rawurlencode($string);
+    }
+
+    /**
+     * Shorthand function for htmlspecialchars($string);
+     */
+    function h($string="") {
+        return htmlspecialchars($string);
+    }
+
+    function error_404() {
+        header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
+        exit();
+    }
+      
+    function error_500() {
+        header($_SERVER["SERVER_PROTOCOL"] . " 500 Internal Server Error");
+        exit();
+    }
+      
+    /**
+     * Function to redirect page
+     */
+    function redirect_to($location) {
+        header("Location: " . $location);
+        exit;
+    }
+    
+    function is_post_request() {
+        return $_SERVER['REQUEST_METHOD'] == 'POST';
+    }
+    
+    function is_get_request() {
+        return $_SERVER['REQUEST_METHOD'] == 'GET';
+    }
+    
+    function display_errors($errors=array()) {
+        $output = '';
+        if(!empty($errors)) {
+            $output .= "<div class=\"errors\">";
+            $output .= "Please fix the following errors:";
+            $output .= "<ul>";
+            foreach($errors as $error) {
+            $output .= "<li>" . h($error) . "</li>";
+            }
+            $output .= "</ul>";
+            $output .= "</div>";
+        }
+        return $output;
+    }
+      
+
+?>
