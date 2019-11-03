dbc1ccbb9521bd75d13ad62a3a1e53f815dfcaf5
Keegan George <kgeorge13@gmail.com>
2019-11-02 22:03:49 -0800

    [Keegan] Included query functions for registering user to database

--- private/query_functions.php
+++ private/query_functions.php
@@ -1,5 +1,92 @@
 <?php
 
 // Include query functions in this file.
+function validate_admin($admin) {
+
+    if (is_blank($admin['first_name'])) {
+        $errors[] = "First name cannot be blank.";
+    } elseif (!has_length($admin['first_name'], array('min' => 2, 'max' => 255))) {
+        $errors[] = "First name must be between 2 and 255 characters.";
+    }
+
+    if (is_blank($admin['last_name'])) {
+        $errors[] = "Last name cannot be blank.";
+    } elseif (!has_length($admin['first_name'], array('min' => 2, 'max' => 255))) {
+        $errors[] = "Last name must be between 2 and 255 characters.";
+    }
+
+    if (is_blank($admin['email'])) {
+        $errors[] = "Email cannot be blank.";
+    } elseif (!has_length($admin['email'], array('max' => 255))) {
+        $errors[] = "Email must be less than 255 characters.";
+    } elseif (!has_valid_email_format($admin['email'])) {
+        $errors[] = "Email must be a valid format.";
+    }
+
+    if (is_blank($admin['username'])) {
+        $errors[] = "Username cannot be blank";
+    } elseif (!has_length($admin['username'], array('min' => 8, 'max' => 255))) {
+        $errors[] = "Username must be between 8 and 255 characters.";
+    } elseif (!has_unique_username($admin['username'], $admin['id'] ?? 0)) {
+        $errors[] = "Username not allowed. Try another.";
+    }
+
+    if(is_blank($admin['password'])) {
+        $errors[] = "Password cannot be blank.";
+      } elseif (!has_length($admin['password'], array('min' => 12))) {
+        $errors[] = "Password must contain 12 or more characters";
+      } elseif (!preg_match('/[A-Z]/', $admin['password'])) {
+        $errors[] = "Password must contain at least 1 uppercase letter";
+      } elseif (!preg_match('/[a-z]/', $admin['password'])) {
+        $errors[] = "Password must contain at least 1 lowercase letter";
+      } elseif (!preg_match('/[0-9]/', $admin['password'])) {
+        $errors[] = "Password must contain at least 1 number";
+      } elseif (!preg_match('/[^A-Za-z0-9\s]/', $admin['password'])) {
+        $errors[] = "Password must contain at least 1 symbol";
+      }
+  
+      if(is_blank($admin['confirm_password'])) {
+        $errors[] = "Confirm password cannot be blank.";
+      } elseif ($admin['password'] !== $admin['confirm_password']) {
+        $errors[] = "Password and confirm password must match.";
+      }
+  
+      return $errors;
+    }
+  
+
+function insert_admin($admin) {
+    global $db;
+    $errors = validate_admin($admin);
+    if (!empty($errors)) {
+        return $errors; 
+    }
+
+    $hashed_password = $admin['password'];
+
+    $sql = "INSERT INTO admins ";
+    $sql .= "(first_name, last_name, email, username, hashed_password) ";
+    $sql .= "VALUES (";
+    $sql .= "'" . db_escape($db, $admin['first_name']) . "',";
+    $sql .= "'" . db_escape($db, $admin['last_name']) . "',";
+    $sql .= "'" . db_escape($db, $admin['email']) . "',";
+    $sql .= "'" . db_escape($db, $admin['username']) . "',";
+    $sql .= "'" . db_escape($db, $hashed_password) . "'";
+    $sql .= ")";
+    $result = mysqli_query($db, $sql);
+
+    // For INSERT statements, $result is true/false
+    if ($result) {
+        return true;
+    } else {
+        // When INSERT fails
+        echo mysqli_error($db);
+        db_disconnect($db);
+        exit;
+    }
+}
+
+
+
 
 ?>
