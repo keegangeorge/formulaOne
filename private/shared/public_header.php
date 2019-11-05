e623f2c151c9be5e5feb4995e0f4e693bc02ddcd
Keegan George <kgeorge13@gmail.com>
2019-11-05 13:50:21 -0800

    [Keegan] Fixed header url bugs

--- private/shared/public_header.php
+++ private/shared/public_header.php
@@ -2,9 +2,10 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8"/>
-		<link rel="apple-touch-icon" sizes="76x76" href="<?php echo url_for('/assets/img/favicon.png'); ?>">
-		<link rel="icon" type="image/png" href="<?php echo url_for('/assets/img/favicon.png'); ?>">
+		<link rel="apple-touch-icon" sizes="76x76" href="<?php echo url_for('assets/img/favicon.png'); ?>">
+		<link rel="icon" type="image/png" href="<?php echo url_for('assets/img/favicon.png'); ?>">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+
 		<title>F1 <?php if (isset($page_title)) { echo '| ' . h($page_title); } ?></title>
         <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport'/>
         
