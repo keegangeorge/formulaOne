22d1e6d32b93fcc373adcc0eb128c373eb8b3df8
Keegan George <kgeorge13@gmail.com>
2019-11-05 13:50:01 -0800

    [Keegan] Updated footer to include logo

--- private/shared/public_footer.php
+++ private/shared/public_footer.php
@@ -1,8 +1,8 @@
 <footer class="bg-primary pt-5 pb-5">
 <div class="container">
 	<div class="row">
-		<div class="col-12 col-md mr-4">
-			<i class="fas fa-copyright text-white"></i>
+		<div class="col-12 col-md mr-4 text-white">
+			<i class="fas fa-flag-checkered fa-3x"></i>
 			<small class="d-block mt-3 mb-4 text-muted">©
 			<script>document.write(new Date().getFullYear())</script>
 			 Keegan George & Apoorva Bitton</a></small>
