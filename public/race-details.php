177f69d613805e94b9a8a369755ff2f2139c2bc6
Keegan George <kgeorge13@gmail.com>
2019-11-05 23:30:10 -0800

    [Keegan] Created race-details page directed by selected race

--- public/race-details.php
+++ public/race-details.php
@@ -1,117 +1,176 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="utf-8"/>
-		<link rel="apple-touch-icon" sizes="76x76" href="./assets/img/favicon.png">
-		<link rel="icon" type="image/png" href="./assets/img/favicon.png">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
-		<title>F1 | Home</title>
-		<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport'/>
-		<!-- Adobe Fonts -->
-		<link rel="stylesheet" href="https://use.typekit.net/qah3bmy.css">
-		<!-- Font Awesome Icons -->
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-		<!-- Main CSS -->
-		<link href="./assets/css/main.css" rel="stylesheet"/>
-		<!-- Animation CSS -->
-		<link href="./assets/css/vendor/aos.css" rel="stylesheet"/>
-	</head>
+<?php require_once '../private/initialize.php'; ?>
 
-<body> 
+<?php $page_title = 'Race Details'; ?>
+<?php include SHARED_PATH . '/public_header.php'; ?>
 
-<?php require("includes/navigation.php"); ?>
+<?php
+
+$raceId = $_GET['raceId'] ?? '1';
+$race = find_race_by_raceId($raceId);
+$circuit = find_circuit($race['circuitId']);
+$race_results = find_results_by_raceId($raceId);
+
+?>
 
 
-<body>
+
+
+<div class="container mt-5 pb-5 pt-3 text-left">
     
-<div class="container pb-5 pt-3 text-left"> <br><br><br><br>  
-        <h1>Race1</h1>
+    <nav class="mt-5" aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="<?php echo url_for('races.php'); ?>">Races</a></li>
+            <li class="breadcrumb-item active" aria-current="page"><?php echo $race['name'] . " (" . $race['year'] . ")"; ?></li>
+        </ol>
+    </nav>
 
-        <div class= "container pt-5">
-            <img class="card-img-top" src="./assets/img/race-details-img/placeholder-race-details-img.png" alt="Card image cap">
+    <!-- Testing some stuffs -->
+    <?php echo $race_results['points'] ?>
+    <!-- End of tests -->
+    
+    <div class="container mt-5">
+        <h5 class="ml-1 text-muted text-uppercase font-weight-light"><?php echo $circuit['name']; ?></h5>
+        <h1 class="text-secondary font-weight-normal">
+            <?php echo $race['name']; ?>
+        </h1>
+        <div class="row text-muted ml-1">
+            <i class="fas fa-calendar-day mr-2"></i>
+            <h6>
+                <?php
+                echo date_format(date_create(h($race['date'])), "M jS, Y") . ' &#x007C; ';
+                ?>
+                <?php
+                if (!is_blank($race['time'])) {
+                    ?>
+                    <i class="fas fa-clock mr-2"></i>
+                <?php
+                    echo date_format(date_create(h($race['time'])), "g:i A") . ' &#x007C; ';
+                }
+                ?>
+                <i class="fas fa-map-marker-alt mr-1"></i>
+
+                <?php
+                echo h($circuit['location']);
+                // When specific location doesn't exist, the comma separator is not displayed
+                if (!is_blank(h($circuit['location']))) {
+                    echo ', ';
+                }
+                ?>
+                <?php
+
+                echo h($circuit['country']);
+                ?>
+            </h6>
         </div>
-        <div class= "container">
 
-            <div class="row pt-5">
-                <h5>Race Location</h5>
+
+    </div>
+
+    <div class="container pt-2">
+        <figure>
+            <img src="../public/assets/img/recent-race-img/<?php echo $circuit['country'] ?>.jpg" alt="">
+        </figure>
+        <div class="col mt-4">
+            <a target="_blank" href="<?php echo $race['url']; ?>" data-toggle="tooltip" data-placement="bottom" data-original-title="View Race Information" class="mt-2 iconbox iconsmall bg-secondary rounded text-white">
+                <i class="fab fa-wikipedia-w"></i>
+            </a>
+
+            <a target="_blank" href="http://google.com/maps/place/
+            <?php echo h($circuit['lat']) . ', ' . h($circuit['lng']); ?>" data-toggle="tooltip" data-placement="bottom" data-original-title="View Circuit Location" class="mt-2 iconbox iconsmall bg-secondary rounded text-white">
+                <i class="fas fa-map-marker-alt"></i>
+            </a>
+        </div>
+        <div class="container">
+
+            <div class="row mt-5">
+                <h5>General Race Information</h5>
+                <?php echo $race['time']; ?>
             </div>
-            <br> 
+            <br>
             <div class="row">
-                <div class= "col-md-3">
+                <div class="col-md-3">
                     <p>Country</p>
                     <h2>Info</h2>
                 </div>
-                <div class= "col-md-3">
+                <div class="col-md-3">
                     <p>Location</p>
                     <h2>Info</h2>
                 </div>
-                <div class= "col-md-3">
+                <div class="col-md-3">
                     <p>Latitude</p>
                     <h2>Info</h2>
                 </div>
-                <div class= "col-md-3">
+                <div class="col-md-3">
                     <p>Longitude</p>
                     <h2>Info</h2>
                 </div>
             </div>
 
-            <br> 
+            <br>
             <hr size="10">
             <br>
-            
+
 
             <div class="row">
                 <h5>Race Results</h5>
             </div>
-            <br> 
+            <br>
             <div class="row">
-                <div class= "col-md-3">
+                <div class="col-md-3">
                     <p>Winner</p>
                     <h2>Info</h2>
                 </div>
-                </div>
+            </div>
 
-            <br> 
+            <br>
             <hr size="10">
             <br>
 
             <div class="row">
-            <h5>Drivers Information</h5>
+                <h5>Drivers Information</h5>
             </div>
-            <br> 
+            <br>
             <div class="row">
-                <div class= "col-md-3">
+                <div class="col-md-3">
                     <p>Names</p>
                     <h2>Info</h2>
                 </div>
-                <div class= "col-md-3">
+                <div class="col-md-3">
                     <p>Standings</p>
                     <h2>Info</h2>
                 </div>
-                <div class= "col-md-3">
+                <div class="col-md-3">
                     <p>Laps Times</p>
                     <h2>Info</h2>
                 </div>
-                <div class= "col-md-3">
+                <div class="col-md-3">
                     <p>Longitude</p>
                     <h2>Info</h2>
                 </div>
             </div>
 
 
-            <br> 
+            <br>
             <hr size="10">
             <br>
-        
-            <div class="row">
-                <h5>Status Information</h5>
-            </div>
-            <br> 
+            <h5>Status Information</h5>
 
+
+            <br>
         </div>
-     </div>
+
+
+    </div>
+</div>
 </div>
 
-</body>
+<?php
 
-</html>
+// mysqli_free_result($season_set);
+// mysqli_free_result($race_set);
+// mysqli_free_result($circuit);
+
+
+?>
+
+<?php include SHARED_PATH . '/public_footer.php'; ?>
