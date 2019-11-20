6d470a3206db229b7ed5a96cc4b895492c0fbf27
apoorvabitton <55360380+apoorvabitton@users.noreply.github.com>
2019-11-19 21:51:43 -0800

    Final race.js

--- public/js/race.js
+++ public/js/race.js
@@ -8,10 +8,10 @@ function setUpSeasonUI() {
         for (var i = 0; i < result.length; i++){
             y = result[i];
             if (y.year == cur_year){
-                $("#seasonDropdownDiv").append(`<li class="dropdown-item selected" id="${y.year}" >${y.year}</a>`);
+                $("#seasonDropdownDiv").append(`<li class="dropdown-item selected" id="${y.year}" >${y.year}</li>`);
             }
             else {
-                $("#seasonDropdownDiv").append(`<li class="dropdown-item" id="${y.year}" >${y.year}</a>`);
+                $("#seasonDropdownDiv").append(`<li class="dropdown-item" id="${y.year}" >${y.year}</li>`);
             }
         }
 
@@ -20,6 +20,8 @@ function setUpSeasonUI() {
             $("#seasonDropdownDiv .dropdown-item").removeClass('selected');
             $(event.target).addClass('selected');
             $("#seasonDropdown").html(event.target.id);
+            $("#dropdownCountryButton").trigger("click");
+            setUpCardDataUI();
             setUpCountryUI();
         });
     });
@@ -28,14 +30,14 @@ function setUpSeasonUI() {
 function setUpCountryUI() {
     $("#dropdownCountryButton").html("Display All");
     $("#countryDropdownSelect").empty();
-    $("#countryDropdownSelect").prepend('<li class="dropdown-item selected" id="Display All" >Display All</a>');
+    $("#countryDropdownSelect").prepend('<li class="dropdown-item selected" id="Display All" >Display All</li>');
     var year = $("#seasonDropdown").html();
 
     $.ajax({method: "GET", url: `./backend/GetRaceByYear.php?year=${year}`}).done(function(data){
         var result= $.parseJSON(data); 
         for (var i = 0; i < result.length; i++){
             var c = result[i];
-            $("#countryDropdownSelect").append(`<li class="dropdown-item" id="${c.country}" >${c.country}</a>`);
+            $("#countryDropdownSelect").append(`<li class="dropdown-item" id="${c.country}" >${c.country}</li>`);
 
         }
     // Set up listener
@@ -43,15 +45,66 @@ function setUpCountryUI() {
         $("#countryDropdownSelect .dropdown-item").removeClass('selected');
         $(event.target).addClass('selected');
         $("#dropdownCountryButton").html(event.target.id);
+        setUpCardDataUI();
     });
 })
 }
 
+function setUpCardDataUI(){
+    
+    // TODO: empty cardUI div
+    $("#card_ui").empty();
+    var search_val = $("#search_by_title").val();
+    var year = $("#seasonDropdown").html();
+    var country = $("#dropdownCountryButton").html();
+    if (country == "Display All"){
+        country = "";
+    }
+    if (search_val.length == 0 || search_val ==  "Enter"){
+        search_val = "";
+    } 
+    $.ajax({method: "GET", url: `./backend/GetRaceCardByYearCountryTitle.php?year=${year}&country=${country}&title=${search_val}`}).done(function(data){
+        var result = $.parseJSON(data);
+        console.log(result);
+        //TODO: add the card ui
+    });
+}
+
+function setSearchBar(){
+    $("#search_by_title").keyup(function(event){
+        var key = event.originalEvent.key;
+        if (key == "Enter") {
+            setUpCardDataUI();
+        } else {
+            $("#suggestionSelect").empty();
+            var search_val = $("#search_by_title").val();
+            if (search_val.length != 0){
+                console.log(search_val);
+                $.ajax({method: "GET", url: `./backend/GetRaceTitleSearch.php?title=${search_val}`}).done(function(data){
+                    console.log(data);
+                    var result = $.parseJSON(data);
+                    for (var i=0; i < result.length; i++){
+                        var n = result[i];
+                        var new_id = n.name.replace(/\s/g, "");
+                        $("#suggestionSelect").append(`<li class="dropdown-item" id="${new_id}" >${n.name}</li>`);
+                        $(`#${new_id}`).click(function(event){
+                            $("#search_by_title").val(n.name);
+                            setUpCardDataUI();
+                        });
+                        $("#search_by_title").trigger("click");
+                    }
+                })
+            }
+            $("#search_by_title").trigger("click");
+        }
+
+    });
+}
 
 // Wait for the page load render
 $(document).ready(function(){
-    $.ajax({method: "GET", url: "./backend/GetRaceTitleSearch.php?title=Aus"}).done(function(data){
-    })
     setUpSeasonUI();
     setUpCountryUI();
+    setSearchBar();
+    setUpCardDataUI();
 });
